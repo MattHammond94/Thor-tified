5b362de0b89dc0246d75ae1cdcf1578c48e3bb3d
Matt Hammond <mjhammond94@gmail.com>
2023-10-09 15:15:34 +0100

    Add imgs to folder and add modal files

---
+++ html-CSS/modals/script.js
@@ -0,0 +1,20 @@
+const container = document.getElementById('container');
+const modalImage = document.getElementById('modal-image');
+const modal = document.getElementById('modal');
+const description = document.getElementById('image-description');
+
+const images = container.querySelectorAll('img');
+
+console.log(images);
+
+for (let i=0; i<images.length; i++) {
+  images[i].addEventListener('click', function() {
+    modalImage.src = images[i].src;
+    description.textContent = images[i].getAttribute('description');
+    modal.classList.remove('no-display')
+  })
+}
+
+modal.addEventListener('click', function() {
+  modal.classList.add('no-display')
+})
