19b102090c02a994fdc90bf1cdad68cde3ca3dc0
Matt Hammond <mjhammond94@gmail.com>
2023-10-12 16:06:34 +0100

    Add Modal component - Add styles to app elements

--- REACT/portals/src/App.js
+++ REACT/portals/src/App.js
@@ -1,9 +1,33 @@
+import { useState } from 'react';
+import Modal from "./Modal";
+
+const BUTTON_WRAPPER_STYLES = {
+  position: 'relative',
+  zIndex: 1
+}
+
+const OTHER_CONTENT_STYLES = {
+  position: 'relative',
+  zIndex: 2,
+  backgroundColor: 'red',
+  padding: '10px'
+}
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-   <div className="container">
-      Portals
-   </div>
+    <>
+      <div style={BUTTON_WRAPPER_STYLES}>
+          <button>Open Modal</button>
+
+          <Modal open={isOpen}>
+            Fancy Modal
+          </Modal>
+      </div>
+
+      <div style={OTHER_CONTENT_STYLES}>Other Content</div>
+    </>
   );
 }
 
