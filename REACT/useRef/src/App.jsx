c34237e776a01a55797b1958eaa05eb50939bd95
Matt Hammond <mjhammond94@gmail.com>
2023-11-04 16:53:25 +0000

    Add useRef hook

--- REACT/useRef/src/App.jsx
+++ REACT/useRef/src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 function App() {
   const [name, setName] = useState('');
+  const renderCount = useRef(0);
+
+  // A use ref returns an object which would look like this:
+  // { current: 0 }
+  
+  // If you used a useEffect here you would cause an infinite loop.
+  // useEffect(() => {
+  //   setRenderCount(prevRenderCount => + 1)
+  // });
 
   return (
     <>
       <input value={name} onChange={e => setName(e.target.value)} />
       <div>My name is {name}</div>
+      <div>I rendered {renderCount} times</div>
     </>
   )
 }
