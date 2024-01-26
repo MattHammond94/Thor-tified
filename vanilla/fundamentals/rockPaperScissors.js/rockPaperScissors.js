b5e60c65a9aab6d78fd1d4f3a86f2d6244c6a19b
Matt Hammond <mjhammond94@gmail.com>
2024-01-26 21:25:33 +0000

    Update rockPaperScissor solution

--- vanilla/fundamentals/rockPaperScissors.js/rockPaperScissors.js
+++ vanilla/fundamentals/rockPaperScissors.js/rockPaperScissors.js
@@ -8,7 +8,9 @@ playRound = (playerSelection, computerSelection) => {
   console.log(computerSelection);
 
   if (playerSelection === computerSelection) {
-    return "Draw";
+    // return "Draw";
+    const playerSelection = prompt();
+    return playRound(playerSelection, getComputerChoice());
   } else if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection == 'rock') {
     return 'You lose!'
   } else {
@@ -16,7 +18,21 @@ playRound = (playerSelection, computerSelection) => {
   }
 }
 
-console.log(playRound('rock', getComputerChoice()));
-console.log(playRound('rock', getComputerChoice()));
-console.log(playRound('rock', getComputerChoice()));
-console.log(playRound('rock', getComputerChoice()));
+// console.log(playRound('rock', getComputerChoice()));
+// console.log(playRound('rock', getComputerChoice()));
+// console.log(playRound('rock', getComputerChoice()));
+// console.log(playRound('rock', getComputerChoice()));
+
+const game = () => {
+  let playerScore = 0
+  let computerScore = 0
+
+  for (let i = 0; i < 5; i++) {
+    // Would need to sanitize user inputs here -> will likely force the input being passed as first arg via a button for each possible choice.
+    playRound(prompt(), getComputerChoice()) === 'You win!' ? playerScore += 1 : computerScore += 1
+  }
+
+  return playerScore > computerScore ? `You win! with a score of ${playerScore}` : `The computer wins! with a score of ${computerScore}`
+}
+
+console.log(game());
