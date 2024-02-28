939dea3eab98a7d2d99b427c27fe3c0fce096190
Matt Hammond <mjhammond94@gmail.com>
2024-02-28 22:44:30 +0000

    Add rainbow mode functionality

--- vanilla/fundamentals/etchASketch/etchASketch.js
+++ vanilla/fundamentals/etchASketch/etchASketch.js
@@ -1,5 +1,6 @@
 const grid = document.querySelector('.grid');
 const clearButton = document.querySelector('.clearBtn');
+let rainbowMode = false;
 
 clearButton.addEventListener("click", () => {
   clearGrid();
@@ -8,6 +9,7 @@ clearButton.addEventListener("click", () => {
 function makeGrid(scale) {
   let pixelSize = 100 / scale;
 
+  console.log(rainbowMode);
   for (let i = 0; i < scale * scale; i++) {
     let pixel = document.createElement('div');
     pixel.className = 'pixel';
@@ -15,20 +17,34 @@ function makeGrid(scale) {
     pixel.style.height = `${pixelSize}%`;
     grid.appendChild(pixel);
 
-    pixel.addEventListener("mouseover", (event) => {
-      pixel.style.backgroundColor = 'black';
+    pixel.addEventListener("mouseover", () => {
+      if (rainbowMode) {
+        pixel.style.backgroundColor = selectColour();
+      } else {
+        pixel.style.backgroundColor = 'black';
+      }
     })
   }
 }
 
 function clearGrid() {
-  console.log("Clear button clicked");
-
   let allPixels = document.querySelectorAll(".pixel");
-  
+
   allPixels.forEach((pixel) => 
     { pixel.style.backgroundColor = 'lightsteelblue'}
   );
+}
+
+function activeRainbowMode() {
+  return rainbowMode === false ? rainbowMode = true : rainbowMode = false
+}
+
+function selectColour() {
+  return `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`
+}
+
+function randomNumber() {
+  return Math.floor(Math.random() * 255);
 }
 
 makeGrid(50)
