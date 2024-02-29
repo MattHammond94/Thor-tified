38c68f323bf19819d8869c09b4964c827a360055
Matt Hammond <mjhammond94@gmail.com>
2024-02-29 20:19:22 +0000

    Add solution for calc add functionality - add delete and clear logic - update HTML and styling

--- vanilla/fundamentals/calculator/calculator.js
+++ vanilla/fundamentals/calculator/calculator.js
@@ -1,13 +1,64 @@
-const display = document.querySelector('.viewPort');
+const display = document.querySelector('.displayNumber');
+const numberButtons = document.querySelectorAll('.valueBtn');
+const operatorButtons = document.querySelectorAll('.operatorBtn');
 
-function addNumberToDisplay(number) {
-  const newNumber = document.createElement('p');
-  newNumber.innerHTML = number;
-  return display.appendChild(newNumber);
+operatorButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    selectOperator(button.innerHTML);
+  });
+});
+
+numberButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    selectNumber(button.innerHTML);
+  });
+});
+
+let currentValue = '';
+let secondValue = '';
+let currentOperation = '';
+
+function selectOperator(operation) {
+  console.log(`this is the operator: ${operation}`);
+  currentOperation = operation;
+  console.log(currentOperation);
 }
 
-function clearDisplay() {
+function selectNumber(number) {
+  if (currentOperation === '+') {
+    secondValue += number
+    return display.innerHTML = secondValue
+  }
+
+  display.innerHTML += number;
+  return currentValue = display.innerHTML;
+}
+
+function equals() {
+  let chosenOperation = null;
+
+  if (currentOperation === '+') {
+    chosenOperation = add
+  }
+
+  let sumValue = completeOperation(parseInt(currentValue), parseInt(secondValue), chosenOperation);
+  display.innerHTML = sumValue;
+  currentValue = sumValue;
+  currentOperation = '';
+  secondValue = '';
+  return;
+}
+
+function clearNumbers() {
   display.innerHTML = ''
+  currentValue = '';
+  secondValue = '';
+}
+
+function deleteLastNumber() {
+  display.innerHTML = display.innerHTML.slice(0, -1);
+  currentValue = display.innerHTML;
+  console.log(currentValue);
 }
 
 function add(num1, num2) {
@@ -26,8 +77,6 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-function completeOpertion(firstNum, secondNum, operation) {
+function completeOperation(firstNum, secondNum, operation) {
   return operation(firstNum, secondNum);
 }
-
-console.log(completeOpertion(20, 10, subtract));
