b11573ebb15368031e47d22b550b54e5b94de433
Matt Hammond <mjhammond94@gmail.com>
2024-02-29 12:31:35 +0000

    Add operation functions to script file

--- vanilla/fundamentals/calculator/calculator.js
+++ vanilla/fundamentals/calculator/calculator.js
@@ -1,1 +1,21 @@
-console.log('Script is working!');
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function completeOpertion(firstNum, secondNum, operation) {
+  return operation(firstNum, secondNum);
+}
+
+console.log(completeOpertion(20, 10, subtract));
